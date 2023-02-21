6d3d96d3e50a982b501345e88d755edfeb6357a5
thongtran2910 <bathong2910@gmail.com>
2023-02-21 13:36:25 +0700

    responsive for mobile and tablet

--- src/routes/navigation/NavigationComponent.js
+++ src/routes/navigation/NavigationComponent.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as SnowmanLogo } from "../../assets/snowman-logo.svg";
+import { ReactComponent as ToggleMenu } from "../../assets/toggle-menu.svg";
 import CartDropdownComponent from "../../components/cart-dropdown/CartDropdownComponent";
 import CartIconComponent from "../../components/cart-icon/CartIconComponent";
+import useWindowDimensions from "../../Hook/useWindowDimensions";
 import { selectIsCartOpen } from "../../store/cart/cartSelector";
 import { selectCurrentUser } from "../../store/user/userSelector";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
@@ -12,6 +14,36 @@ import "./navigation.scss";
 export default function NavigationComponent() {
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
+  const { width } = useWindowDimensions();
+  const [isShowing, setIsShowing] = useState(false);
+
+  const onChangeStyleHandler = () => {
+    setIsShowing(!isShowing);
+  };
+
+  const handleCloseMenu = () => {
+    setIsShowing(false);
+  };
+
+  const handleSignoutUser = () => {
+    signOutUser();
+    setIsShowing(false);
+  };
+
+  useEffect(() => {
+    const onDimensionsChange = () => {
+      if (width > 1023) {
+        setIsShowing(false);
+      }
+    };
+    onDimensionsChange();
+  }, [width]);
+
+  window.onclick = function (event) {
+    if (event.target.id !== "toggle") {
+      setIsShowing(false);
+    }
+  };
 
   return (
     <>
@@ -22,25 +54,49 @@ export default function NavigationComponent() {
         <Link className="nav-brand" to="/">
           Snowman Clothing
         </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
-            SHOP
-          </Link>
-          {currentUser ? (
-            <>
-              <span>Hi! {currentUser.displayName}</span>
-              <span className="nav-link" onClick={signOutUser}>
-                SIGN OUT
-              </span>
-            </>
-          ) : (
-            <Link className="nav-link" to="/auth">
-              SIGN IN
+        <div className="nav-toggle-container">
+          <ToggleMenu
+            id="toggle"
+            className="nav-toggle"
+            onClick={onChangeStyleHandler}
+          />
+          <div
+            style={{ display: isShowing ? "block" : "none" }}
+            className="nav-link-mobile"
+          >
+            <Link onClick={handleCloseMenu} className="nav-link" to="/shop">
+              SHOP
             </Link>
-          )}
-          <CartIconComponent />
+            {currentUser ? (
+              <Link to="/auth" className="nav-link" onClick={handleSignoutUser}>
+                <p>SIGN OUT</p>
+              </Link>
+            ) : (
+              <Link onClick={handleCloseMenu} className="nav-link" to="/auth">
+                <p>SIGN IN</p>
+              </Link>
+            )}
+          </div>
+          <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+              SHOP
+            </Link>
+            {currentUser ? (
+              <>
+                <p className="nav-link">Hi! {currentUser.displayName}</p>
+                <Link to="/auth" className="nav-link" onClick={signOutUser}>
+                  SIGN OUT
+                </Link>
+              </>
+            ) : (
+              <Link className="nav-link" to="/auth">
+                SIGN IN
+              </Link>
+            )}
+          </div>
+          <CartIconComponent className="cart-icon" />
+          {isCartOpen && <CartDropdownComponent />}
         </div>
-        {isCartOpen && <CartDropdownComponent />}
       </div>
       <Outlet />
     </>
